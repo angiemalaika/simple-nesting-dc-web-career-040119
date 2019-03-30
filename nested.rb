9372ccd5752d731c3ff5374ae1942409b4708071
Angela Mumba <malaika.mumba@gmail.com>
2019-03-30 01:18:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -5,7 +5,16 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
+        }
+ 		}
+ 		
+ 		return programmer_hash[:grace_hopper]
+ 		
+ 		end
+ 		
+ 	def alan_kay_is_known_for
+ 	  
+ 	  programmer_hash {
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
