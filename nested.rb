f040ef08794423343695137ea5f08c88798cea64
Angela Mumba <malaika.mumba@gmail.com>
2019-03-30 01:27:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,7 +1,13 @@
 
 def hopper
 	programmer_hash = 
+	
  		{
+ 		  
+ 		  :yukihiro_matsumoto => {
+ :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
